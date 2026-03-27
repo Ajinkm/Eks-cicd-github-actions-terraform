170f73ec193f97961bb3b8afbe135be1e673fba6
Ajinkm <118746844+Ajinkm@users.noreply.github.com>
2026-03-27 17:27:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,18 +4,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 4.67"
     }
   }
 }
 
+#################################
+# AWS Provider
+#################################
 provider "aws" {
   region = var.region
 }
 
-############################
-# VPC
-############################
+#################################
+# VPC MODULE
+#################################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
@@ -23,7 +26,7 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
@@ -38,9 +41,9 @@ module "vpc" {
   }
 }
 
-############################
-# EKS Cluster
-############################
+#################################
+# EKS MODULE (COMPATIBLE VERSION)
+#################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.0.0"
@@ -51,12 +54,18 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  enable_irsa = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
-      desired_size   = 2
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
+
   tags = {
     Environment = "dev"
     Project     = "eks-cicd"
