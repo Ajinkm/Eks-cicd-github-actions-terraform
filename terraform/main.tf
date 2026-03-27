9adfa8f702a8d9b4981ef54047805d7a895d9520
Ajinkm <118746844+Ajinkm@users.noreply.github.com>
2026-03-27 16:19:54 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = var.region
+}
+
+############################
+# VPC
+############################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Environment = "dev"
+    Project     = "eks-cicd"
+  }
+}
+
+############################
+# EKS Cluster
+############################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "eks-cicd"
+  }
+}
